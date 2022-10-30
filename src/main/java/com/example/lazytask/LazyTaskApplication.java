289549ead2cc3b06e00e8219db2745c3224d9aba
Henning Poettker <Henning.Poettker@c24.de>
2022-10-30 02:42:14 +0200

    init commit

---
+++ src/main/java/com/example/lazytask/LazyTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lazytask/LazyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
